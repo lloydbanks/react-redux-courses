a23561af7f2be41d69e2ba8fc1c7109c8a4415f3
Dmitry <dmitry.litvinov.spb@gmail.com>
2019-11-18 21:59:36 +0300

    add detail course fetch

---
+++ src/components/404/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFoundPage = () => <h1 className="text-danger">404 Not Found</h1>
+
+export default NotFoundPage
--- src/components/courses/detail.js
+++ src/components/courses/detail.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import NotFound from '../404'
 
-const CourseDetail = ({id}) => {
-    return <div>Viewing {id}</div>
+const CourseDetail = ({id, course, loading}) => {
+    if(loading) return <div>Loading...</div>
+    if(!course) return <NotFound />
+
+    return <div>Viewing {course.title}</div>
 }
 
-export default CourseDetail
+const mapState = (state, props) => {
+    return {
+        loading: state.courses.loading,
+        course: state.courses.courses.find(
+            c => c.id === +props.id
+        )
+    }
+}
+
+export default connect(mapState)(CourseDetail)
