207d1f7c55b80f42447cc97408bf66e795856b9f
Dmitry <dmitry.litvinov.spb@gmail.com>
2019-11-18 22:25:49 +0300

    add detail style

--- src/components/courses/detail.js
+++ src/components/courses/detail.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import NotFound from '../404'
+import {Link} from '@reach/router'
 
 const CourseDetail = ({id, course, loading}) => {
     if(loading) return <div>Loading...</div>
     if(!course) return <NotFound />
 
-    return <div>Viewing {course.title}</div>
+    return (
+        <div className="card mb-3">
+            <div className="row no-gutters">
+                <div className="col-md-12">
+                    <div className="card-body">
+                        <h5 className="card-title">{course.title}</h5>
+                        <p className="card-text">Price: ${course.price}</p>
+                        <Link to="/courses" className="btn btn-primary">View all</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 const mapState = (state, props) => {
